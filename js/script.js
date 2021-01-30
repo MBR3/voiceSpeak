8efb946aeb1d0ebbe43aed76af0ca891a39b00d6
= <=>
2021-01-30 10:32:27 +0900

    [add]テキストエリア内の文字をしゃべるようにした

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
-function loadVoices() {
-    var voices = window.speechSynthesis
-        .getVoices()
-        .map(x => [x.name, x.lang].join("\t"))
-        .join("\r\n");
-    alert(voices)
-    };
-    
-
-loadVoices();
-
-window.speechSynthesis.onvoiceschanged = function(e) { loadVoices(); }
+document.getElementById("exec").onclick = function() {
+    let uttearnce = new SpeechSynthesisUtterance();
+    uttearnce.text = document.getElementById("text").value;
+    console.log(uttearnce.text)
+    uttearnce.volume = 1;
+    uttearnce.rate = 1;
+    uttearnce.pitch = 1;
+    uttearnce.lang = 'ja-JP'
+    window.speechSynthesis.speak(uttearnce);
+}
